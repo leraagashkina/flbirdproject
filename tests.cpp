30126bca2e7f666171fb4a48787fd4e3f48d7d74
leraagashkina <vaagashkina@miem.hse.ru>
2022-06-18 02:02:31 +0300

    Initial commit

---
+++ tests.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//#include <cassert>
+//#include "main.cpp"
+
+//void TestCollision() {
+//    pipePos[0] = 60;
+//    birdPos = 0;
+//    gapPos[0] = 0;
+//    assert(collision() == 0);
+//    cout << "Test OK" << endl;
+//}
+
+//void TestReaction() {
+//    cout << "Когда попросят, нажмите esc";
+//    assert(reaction() == -1);
+//    cout << "Test OK" << endl;
+//}
+
+//int mainTest() {
+//    TestCollision();
+//    return 0;
+//}
