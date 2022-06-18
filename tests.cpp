27257eb268d7fce26d6f25cd19da356c7bfaf113
leraagashkina <vaagashkina@miem.hse.ru>
2022-06-18 14:09:43 +0300

    vers2

--- tests.cpp
+++ tests.cpp
@@ -1,22 +1,23 @@
-//#include <iostream>
-//#include <cassert>
-//#include "main.cpp"
+#include <iostream>
+#include <doctest.h>
+#include "flappybird.h"
 
-//void TestCollision() {
-//    pipePos[0] = 60;
-//    birdPos = 0;
-//    gapPos[0] = 0;
-//    assert(collision() == 0);
-//    cout << "Test OK" << endl;
-//}
+TEST_CASE("Collision") {
+REQUIRE(collision(60, 0, 0) == 0);
+REQUIRE(collision(65, 7, 6) == 1);
+REQUIRE(collision(65, 3, 11) == 1);
+}
 
-//void TestReaction() {
-//    cout << "Когда попросят, нажмите esc";
-//    assert(reaction() == -1);
-//    cout << "Test OK" << endl;
-//}
+TEST_CASE("The bird up") {
+REQUIRE(pressSpase(6) == 3);
+REQUIRE(pressSpase(2) == 2);
+}
 
-//int mainTest() {
-//    TestCollision();
-//    return 0;
-//}
+TEST_CASE("The bird down") {
+REQUIRE(birdDown(6) == 7);
+}
+
+// Для проверки всех ошибок
+extern "C" const char *__asan_default_options() {
+    return "debug=1:detect_invalid_pointer_pairs=2:detect_leak=1:detect_leaks=1:leak_check_at_exit=true:color=always";
+}
